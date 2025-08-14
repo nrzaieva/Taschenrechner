f48c6c12e23ef42828886b42c5d0e63a6d328ca9
Rzaieva <NRzaieva@bleckmann.de>
2025-08-14 12:57:56 +0200

    switch durch if-else ersetzt

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const divButton = document.getElementById('dividieren');
 addButton.addEventListener('click', () => {
     selectedOperation = 'addieren';
     document.getElementById('operation').innerText = selectedOperation;
-    
 });
 
 subButton.addEventListener('click', () => {
@@ -32,7 +31,7 @@ divButton.addEventListener('click', () => {
 });
 
 document.getElementById('berechnen').addEventListener('click', (e) => {
-    //e.preventDefault(); //seite aktualisierung abbrechen
+
 
     if (!selectedOperation) {
         alert('Bitte wählen Sie eine Operation aus.');
@@ -45,20 +44,6 @@ document.getElementById('berechnen').addEventListener('click', (e) => {
 });
 
 function berechneErgebnis() {
-    // "1+2"
-    // "2-1"
-    // "2*3"
-    // "6/3"
-
-    // 2. Schritt (bis maximal 7 Stellige Zahlen) iterators:
-    // "10+26"
-    // "25-178"
-    // "2*358"
-    // "600/3"
-
-    // 3. Schritt beliebig viele addiition und subtraktions Operationen hintereinander unterstützen:
-    // "1+2-3+4-5"
-    // "10+20-30+40-50"
 
     const zahl1 = parseFloat(document.getElementById('zahl-1').value);
     const zahl2 = parseFloat(document.getElementById('zahl-2').value);
@@ -71,27 +56,21 @@ function berechneErgebnis() {
 
     let ergebnis;
 
-    switch (selectedOperation) {
-        case 'addieren':
-            ergebnis = zahl1 + zahl2;
-            break;
-        case 'subtrahieren':
-            ergebnis = zahl1 - zahl2;
-            break;
-        case 'multiplizieren':
-            ergebnis = zahl1 * zahl2;
-            break;
-        case 'dividieren':
-            if (zahl2 === 0) {
-                alert('Division durch null ist nicht erlaubt.');
-                return;
+
+            if (selectedOperation === 'addieren') {
+                ergebnis = zahl1 + zahl2;
+            } else if (selectedOperation === 'subtrahieren') {
+                ergebnis = zahl1 - zahl2;
+            } else if (selectedOperation === 'multiplizieren') {
+                ergebnis = zahl1 * zahl2;
+            } else if (selectedOperation === 'dividieren') {
+                if (zahl2 === 0) {
+                    alert('Division durch null ist nicht erlaubt.');
+                    return;
+                }
+                ergebnis = zahl1 / zahl2;
             }
-            ergebnis = zahl1 / zahl2;
-            break;
-        default:
-            alert('Bitte wählen Sie eine Rechenoperation aus.');
-            return;
-    }
+            
 
     ergebnisFeld.value = ergebnis;
 }
