3d404589bd5232e3686c214d06cf96e5b05dee44
Nadiia Rzaieva <nrzaieva@bleckmann.de>
2025-08-15 14:12:12 +0200

    Tippfehler gelöst

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ button.addEventListener("click", calculateInput);
 
 function calculateInput() {
     /* was der Benutzer eingegeben hat */
-    const input = documentGetElementById("input").value;
+    const input = document.getElementById("input").value;
 
     /* array von Zahlen und Zeichen */
     const tokens = parseInput(input);
@@ -16,3 +16,32 @@ function calculateInput() {
     document.getElementById("ergebnis").value = result;
 }
   
+function parseInput(input) {
+    const regex = /(\d+|\+|\-)/g; /* global */
+    return input.match(regex).map(token => {/*{ !isNaN(token) ? Number(token) : token; });*/
+        if (!isNaN(token)) {
+            return Number(token);
+        } else {
+            return token;
+        }
+    });
+}
+
+    function simpleCalculate(tokens) {
+        let result = 0;
+        let currentOperator = "+";
+
+        tockens.forEach(token => {
+            if (typeof token === "number") {
+                if (currentOperator === "+") {
+                    result += token;
+                } else if (currentOperator === "-") {
+                    result -= token;
+                }
+                } else if (typeof token === "string") {
+                    currentOperator = token;
+                }
+        });
+        return result;
+
+    }
