9ca082a02fdc00f8289fe3f03e316de926feea36
Nadiia Rzaieva <nrzaieva@bleckmann.de>
2025-08-18 10:36:25 +0200

    die Anzahl der Zeichen und Noch *  / hinzugefügt

--- script.js
+++ script.js
@@ -18,18 +18,27 @@ function calculateInput() {
   
 /* global */
 function parseInput(input) {
-    const regex = /(\d+|\+|\-)/g; 
+    // Noch * und / hinzugefügt
+    const regex = /(\d+|\+|\-|\*|\/)/g;
     return input.match(regex).map(token => { 
         if (!isNaN(token)) {
-            return Number(token);
+            const number = Number(token);
+            // Zählt die Anzahl der Zeichen
+            if (number.toString().length > 7) {
+                alert("Nur Zahlen mit maximal 7 Stellen erlaubt!");
+                throw new Error("Zahl zu lang");
+            }
+            return number;
         } else {
             return token;
         }
     });
+    return tokens;
 }
 /*{ !isNaN(token) ? Number(token) : token; });*/
 
     function simpleCalculate(tokens) {
+        // Erste Zahl
         let result = 0;
         let currentOperator = "+";
 
